22c52b79c60fa4796c4f4eab8afa097cb4503eb0
Dean B <dean@deanveloper.com>
2016-10-22 23:45:34 -0400

    Added simplification of functions

--- src/main/kotlin/com/deanveloper/derivativekt/Main.kt
+++ src/main/kotlin/com/deanveloper/derivativekt/Main.kt
@@ -1,19 +1,23 @@
 package com.deanveloper.derivativekt
 
+import com.deanveloper.derivativekt.expression.AdditionExpression
+import com.deanveloper.derivativekt.expression.MultiplicationExpression
+import com.deanveloper.derivativekt.expression.Value
+import com.deanveloper.derivativekt.expression.Variable
 import java.math.BigDecimal
 
 /**
  * @author Dean
  */
 fun main(vararg args: String) {
-    println(
-            ExponentialExpression(charArrayOf('x'),
-                    AdditionExpression(
-                            charArrayOf('x'),
-                            Value(BigDecimal.valueOf(5)),
-                            Variable('x')
-                    ),
-                    Variable('x')
-            ).invoke(Value(BigDecimal.valueOf(5)))
+    val exp = AdditionExpression('x',
+            MultiplicationExpression('x', Value(BigDecimal.ONE), Variable('x')),
+            MultiplicationExpression('x', Value(BigDecimal.ZERO), Variable('x'))
     )
+
+    println(exp)
+    println(exp.simplify())
+    println(exp.derive())
+    println(exp.derive().simplify())
+    println(exp.simplify().derive())
 }
